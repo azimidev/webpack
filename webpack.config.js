abfd7b3368a5bb3b96e5b5f5f888ded4bb452247
Hassan Azimi <hazz.azimi@gmail.com>
2018-02-20 21:51:34 +0000

    PurifyCss

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,61 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack           = require('webpack');
+var path              = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var inProduction = (process.env.NODE_ENV === 'production');
+var glob              = require('glob');
+var PurifyCSSPlugin   = require('purifycss-webpack');
+var inProduction      = (process.env.NODE_ENV === 'production');
 
 module.exports = {
-    entry: {
-        app: [
-            './src/main.js',
-            './src/main.scss'
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: '[name].js'
-    },
-    module: {
-        rules: [{
-                test: /\.s[ac]ss$/,
-                use: ExtractTextPlugin.extract({
-                    loader: ["css-loader", "sass-loader"],
-                    fallback: "style-loader",
-                })
-            },
-            {
-                test: /\(.png|jpe?g|gif|svg|bmp|eot|ttf|woff|woff2)$/,
-                loader: "file-loader",
-                options: {
-                    name: '[name].[hash].[ext]'
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: '/node_modules/',
-                use: 'babel-loader'
-            },
-        ],
-    },
-    plugins: [
-        // new webpack.optimize.UglifyJsPlugin()
-        new ExtractTextPlugin("[name].css"),
+	entry   : {
+		app : [
+			'./src/main.js',
+			'./src/main.scss',
+		],
+	},
+	output  : {
+		path     : path.resolve(__dirname, './dist'),
+		filename : '[name].js',
+	},
+	module  : {
+		rules : [
+			{
+				test : /\.s[ac]ss$/,
+				use  : ExtractTextPlugin.extract({
+					loader   : ["css-loader", "sass-loader"],
+					fallback : "style-loader",
+				}),
+			},
+			{
+				test    : /\.(png|jpe?g|gif|svg|bmp|eot|ttf|woff|woff2)$/,
+				loader  : "file-loader",
+				options : {
+					name : '[name].[hash].[ext]',
+				},
+			},
+			{
+				test    : /\.js$/,
+				exclude : '/node_modules/',
+				use     : 'babel-loader',
+			},
+		],
+	},
+	plugins : [
+		// new webpack.optimize.UglifyJsPlugin()
+		new ExtractTextPlugin("[name].css"),
 
-        new webpack.LoaderOptionsPlugin({
-            minimize: inProduction
-        })
-    ],
+		new PurifyCSSPlugin({
+			paths    : glob.sync(path.join(__dirname, 'index.html')),
+			minimize : inProduction,
+		}),
+
+		new webpack.LoaderOptionsPlugin({
+			minimize : inProduction,
+		}),
+	],
 };
 
 if (inProduction) {
-    module.exports.plugins.push(
-        new webpack.optimize.UglifyJsPlugin()
-    );
+	module.exports.plugins.push(
+		new webpack.optimize.UglifyJsPlugin(),
+	);
 }
